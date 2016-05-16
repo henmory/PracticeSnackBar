e2956b772b44d535eac55d3a4d49cac032f477bc
henmory <hanmh87@163.com>
2016-05-16 21:28:45 +0800

    1练习snackbar 2.软件自建的项目中就有，在短信标签地下 3.这是安卓替代Toast的 4.需要在布局文件的最外层包装上CoordinatorLayout这个布局 5.再用sanckbar的make方法构造对象 6.调用setaction方法设置点击事件 7.show方法显示

---
+++ app/src/main/java/com/henmory/practicesnackbar/Main2Activity.java
@@ -0,0 +1,42 @@
+package com.henmory.practicesnackbar;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void test_snack(View view) {
+        final Snackbar snackbar = Snackbar.make(findViewById(R.id.cd_layout), "test", Snackbar.LENGTH_LONG);
+        snackbar.setAction("new action", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        snackbar.show();
+    }
+
+}
